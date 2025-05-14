9775336e81dea9a27b3ebde25c3dfd6d7d7e5f4f
flornkm <florian.kiem@hfg.design>
2025-05-15 01:31:13 +0200

    feat: initial release

---
+++ demo/src/app.tsx
@@ -0,0 +1,63 @@
+import Copier from "./components/copier";
+import { CodeIcon, ComponentIcon } from "./components/icon";
+
+function App() {
+  return (
+    <main className="min-h-screen px-4">
+      <div className="w-full max-w-5xl mx-auto pb-24">
+        <div className="overflow-hidden rounded-b-3xl w-full h-96 mb-8">
+          <img src="/images/sky.webp" alt="Robot" className="w-full object-cover h-full dither " />
+        </div>
+        <h1 className="text-lg font-semibold mb-1.5 leading-tight">
+          The easiest way to use dither effects in the web
+        </h1>
+        <p className="text-sm text-neutral-500 mb-6">
+          The dither plugin for TailwindCSS is adding a dither effect to your elements by using
+          simple SVG filters.
+        </p>
+        <div className="flex flex-col justify-between items-start gap-8 mb-12">
+          <Copier packageManagers={["npm", "yarn", "pnpm"]} />
+          <div>
+            <p className="text-sm mb-2 text-neutral-500">
+              …and then import into your <CodeIcon className="inline-block w-3 ml-1 mb-0.5" />{" "}
+              stylesheet:
+            </p>
+            <div className="font-mono text-sm bg-neutral-100 py-4 px-6 rounded-lg">
+              import "dither-plugin"
+            </div>
+          </div>
+          <div>
+            <p className="text-sm mb-2 text-neutral-500">
+              …and use in your <ComponentIcon className="inline-block w-3 ml-1 mb-0.5" /> components
+              like this:
+            </p>
+            <div className="font-mono text-sm bg-neutral-100 py-4 px-6 rounded-lg">
+              &lt;Component className="dither" /&gt;
+            </div>
+          </div>
+        </div>
+        <h2 className="font-semibold mb-2">Scroll for examples</h2>
+        <div className="gap-4 flex flex-col">
+          <div className="overflow-hidden rounded-xl w-full aspect-square">
+            <img
+              src="/images/flower.webp"
+              alt="Flower"
+              className="w-full h-full object-cover object-center dither"
+            />
+          </div>
+          <div className="overflow-hidden rounded-xl w-full h-96">
+            <video
+              src="/videos/pot.mp4"
+              autoPlay
+              muted
+              loop
+              className="w-full h-full object-cover object-top dither"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ demo/src/components/copier.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import Button from "./button";
+import { ChevronDownIcon, CopyIcon } from "./icon";
+
+const Copier = ({ packageManagers }: { packageManagers: string[] }) => {
+  const [copied, setCopied] = useState(false);
+  const [packageManager, setPackageManager] = useState(packageManagers[0]);
+
+  return (
+    <div className="flex w-full md:w-auto justify-between items-center gap-4 p-1 pr-[5px] pl-2 rounded-full border bg-neutral-50 border-neutral-200 text-sm">
+      <div className="flex items-center gap-2">
+        <div className="relative">
+          <select
+            className="font-mono appearance-none rounded-full px-2 h-8 pr-5 outline-none focus:ring-1 focus:ring-neutral-200 transition-all cursor-pointer"
+            value={packageManager}
+            onChange={(e) => setPackageManager(e.target.value)}
+          >
+            {packageManagers.map((packageManager) => (
+              <option key={packageManager} value={packageManager}>
+                {packageManager}
+              </option>
+            ))}
+          </select>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-1 pointer-events-none">
+            <ChevronDownIcon className="w-4 h-4" />
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <p className="font-mono line-clamp-1">install dither-plugin</p>
+        </div>
+      </div>
+      <Button
+        className="py-2 pl-2"
+        onClick={() => {
+          navigator.clipboard.writeText(`${packageManager} install dither-plugin`);
+          setCopied(true);
+          setTimeout(() => setCopied(false), 2000);
+        }}
+      >
+        <CopyIcon className="w-4 h-4" />
+        {copied ? "Copied" : "Copy"}
+      </Button>
+    </div>
+  );
+};
+
+export default Copier;
